b33313e6451d4bd22f8c272e24587cd7c3d3f2fa
Camila Pinzon <camilapinz18@gmail.com>
2023-02-07 10:35:26 -0500

    Integracion index.js y html

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ const app = Vue.createApp({
       cantidadIngresada: null,
       bodegaSeleccionada: null,
       unidadSeleccionada: null,
-      unidadesMedida: {}
+      unidadesMedida: {},
+      isBienvenida: false,
+      isCompra: false
     }
   },
 
   methods: {
     alCargarPagina () {
       localStorage.setItem('inventario', JSON.stringify(this.inventario))
+      this.isBienvenida = true
+    },
+    irAComprar () {
+      ;(this.isBienvenida = false), (this.isCompra = true)
     },
     actualizarLocalStorage (actualizacion) {
       let inventarioActualizar = JSON.parse(localStorage.getItem('inventario'))
@@ -40,47 +46,52 @@ const app = Vue.createApp({
       localStorage.setItem('inventario', JSON.stringify(inventarioActualizar))
     },
     convertirCantidad () {
-      if (this.unidadSeleccionada == 'kilos') {
-      } else if (this.unidadSeleccionada == 'libras') {
+      if (this.unidadSeleccionada == 'kg') {
+      } else if (this.unidadSeleccionada == 'lb') {
         this.cantidadIngresada = this.cantidadIngresada / 2
         console.log(this.cantidadIngresada, 'CONVERSION LIBRAS')
-      } else if (this.unidadSeleccionada == 'gramos') {
+      } else if (this.unidadSeleccionada == 'g') {
         this.cantidadIngresada = this.cantidadIngresada / 1000
         console.log(this.cantidadIngresada, 'CONVERSION GRAMOS')
       }
     },
     realizarCompra () {
-      if (this.bodegaSeleccionada === '1') {
-        console.log('b1:', this.bodegaSeleccionada)
-        const inventarioModificar = this.inventario.find(
-          bodega => bodega.bodega == this.bodegaSeleccionada
-        )
-        this.convertirCantidad()
-        const objetoInventarioModificar = {
-          bodega: inventarioModificar.bodega,
-          cantidad: (inventarioModificar.cantidad =
-            inventarioModificar.cantidad - this.cantidadIngresada)
+      if (this.cantidadIngresada === '' || this.cantidadIngresada <= 0) {
+        alert('Datos invalidos. No puede ser un menor menor o igual a 0')
+        this.cantidadIngresada = ''
+      } else {
+        if (this.bodegaSeleccionada === '1') {
+          console.log('b1:', this.bodegaSeleccionada)
+          const inventarioModificar = this.inventario.find(
+            bodega => bodega.bodega == this.bodegaSeleccionada
+          )
+          this.convertirCantidad()
+          const objetoInventarioModificar = {
+            bodega: inventarioModificar.bodega,
+            cantidad: (inventarioModificar.cantidad =
+              inventarioModificar.cantidad - this.cantidadIngresada)
+          }
+          this.actualizarLocalStorage(inventarioModificar)
+          console.log('inventarioModificarPostVenta', inventarioModificar)
+          Object.assign(inventarioModificar, objetoInventarioModificar)
+          alert('Compra realizada con exito!')
+          this.cantidadIngresada = ''
+        } else if (this.bodegaSeleccionada === '2') {
+          console.log('b2:', this.bodegaSeleccionada)
+          const inventarioModificar = this.inventario.find(
+            bodega => bodega.bodega == this.bodegaSeleccionada
+          )
+          this.convertirCantidad()
+          inventarioModificar.cantidad =
+            inventarioModificar.cantidad - this.cantidadIngresada
+          this.actualizarLocalStorage(inventarioModificar)
+          console.log(
+            'inventarioModificarPostVenta',
+            inventarioModificar.cantidad
+          )
+          alert('Compra realizada con exito!')
+          this.cantidadIngresada = ''
         }
-
-        this.actualizarLocalStorage(inventarioModificar)
-
-        console.log('inventarioModificarPostVenta', inventarioModificar)
-
-        Object.assign(inventarioModificar, objetoInventarioModificar)
-      } else if (this.bodegaSeleccionada === '2') {
-        console.log('b2:', this.bodegaSeleccionada)
-        const inventarioModificar = this.inventario.find(
-          bodega => bodega.bodega == this.bodegaSeleccionada
-        )
-        this.convertirCantidad()
-        inventarioModificar.cantidad =
-          inventarioModificar.cantidad - this.cantidadIngresada
-
-        this.actualizarLocalStorage(inventarioModificar)
-        console.log(
-          'inventarioModificarPostVenta',
-          inventarioModificar.cantidad
-        )
       }
     }
   },
